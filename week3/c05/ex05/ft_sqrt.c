0c3d7d4175065fde3377a1159ffa0e639f7689e5
BeyondGodMode <tanakorn.devcs@gmail.com>
2023-06-25 11:23:24 +0700

    c05

--- week3/c05/ex05/ft_sqrt.c
+++ week3/c05/ex05/ft_sqrt.c
@@ -6,13 +6,36 @@
 /*   By: beyond <beyond@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 13:01:18 by beyond            #+#    #+#             */
-/*   Updated: 2023/06/22 13:10:10 by beyond           ###   ########.fr       */
+/*   Updated: 2023/06/25 11:15:31 by beyond           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
+// #include <stdio.h>
 
 int	ft_sqrt(int nb)
 {
-	
+	int	guess;
+
+	guess = nb / 2;
+	if (nb <= 0 || nb == 2)
+		return (0);
+	if (nb == 1)
+		return (1);
+	while (guess * guess - nb > 0)
+	{
+		guess = (guess + nb / guess) / 2;
+	}
+	if ((guess * guess) - nb != 0)
+		return (0);
+	return (guess);
 }
+
+// int	main(void)
+// {
+// 	// printf("%d",ft_sqrt(3));
+// 	for (int i = 0 ; i < 125; i++)
+// 	{
+// 		printf("i = :%d Sqrt = %d\n",i, ft_sqrt(i));
+// 	}
+// 	return (0);
+// }
